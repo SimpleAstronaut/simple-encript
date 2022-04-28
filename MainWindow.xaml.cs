640c28636a869085f9b7a1125d6086219911c3df
SimpleAstronaut <63186671+SimpleAstronaut@users.noreply.github.com>
2022-04-28 19:58:53 +0800

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace encript
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        public void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = text.Text.ToString();
+            string ACCkey = key.Text.ToString();
+            string ACCmsg = name + ACCkey;
+            string message = Md5Helper.Md5(ACCmsg);
+            ret.Text = message;
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string ACCmsg = text.Text.ToString() + key.Text.ToString();
+            string message = SHA1(ACCmsg);
+            ret.Text = message;
+        }
+        public static string SHA1(string content)
+        {
+            return SHA1(content, Encoding.UTF8);
+        }
+
+        public static string SHA1(string content, Encoding encode)
+        {
+            try
+            {
+                SHA1 sha1 = new SHA1CryptoServiceProvider();
+                byte[] bytes_in = encode.GetBytes(content);
+                byte[] bytes_out = sha1.ComputeHash(bytes_in);
+                sha1.Dispose();
+                string result = BitConverter.ToString(bytes_out);
+                result = result.Replace("-", "");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("SHA1加密出错：" + ex.Message);
+            }
+        }
+    }
+
+    public class Md5Helper
+    {
+        public static string Md5(string value)
+        {
+            var result = string.Empty;
+            if (string.IsNullOrEmpty(value)) return result;
+            using (var md5 = MD5.Create())
+            {
+                result = GetMd5Hash(md5, value);
+            }
+            return result;
+        }
+
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            var sBuilder = new StringBuilder();
+            foreach (byte t in data)
+            {
+                sBuilder.Append(t.ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+        static bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            var hashOfInput = GetMd5Hash(md5Hash, input);
+            var comparer = StringComparer.OrdinalIgnoreCase;
+            return 0 == comparer.Compare(hashOfInput, hash);
+        }
+    }
+}
